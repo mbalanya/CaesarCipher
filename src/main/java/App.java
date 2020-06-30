d42d3516d34cab5f11e0dc4aee2812a09f40d3cd
mbalanya <okobacharles@yahoo.co.uk>
2020-06-30 19:45:08 +0300

    add messages to the user

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,17 @@
+import java.io.Console;
+
 public class App {
     public static void main(String[] args){
+        Console myConsole = System.console();
+
+        System.out.println("---------------------------");
+        System.out.println("-+-CAESAR CIPHER ENCRYPT-+-");
+        System.out.println("---------------------------");
+        System.out.println("Type e to Encrypt or d to decrypt");
+        System.out.println("Welcome to Encryption. Enter sentence to be Encrypted: ");
+        System.out.println("Your encrypted code is: ");
+        System.out.println("Welcome to Decryption. Enter sentence to be Decrypted: ");
+        System.out.println("Your decrypted code is: ");
 
     }
 }

a38ca2d8d95f1cda007704648f428e79657c9819
mbalanya <okobacharles@yahoo.co.uk>
2020-06-30 20:08:54 +0300

    convert sentence to array of words in app.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,9 +11,20 @@ public class App {
         String entryInput = myConsole.readLine();
         if (entryInput == "e"){
             System.out.println("Welcome to Encryption. Enter sentence to be Encrypted: ");
-            
+            String encryptSentence = myConsole.readLine();
+            encryptSentence = encryptSentence.toLowerCase();
+            String[] words = encryptSentence.split("\\s+");
+            for (int i = 0; i < words.length; i++){
+                words[i] = words[i].replaceAll("^\\w", "");
+            }
         } else if (entryInput == "d"){
             System.out.println("Welcome to Decryption. Enter sentence to be Decrypted: ");
+            String encryptSentence = myConsole.readLine();
+            encryptSentence = encryptSentence.toLowerCase();
+            String[] words = encryptSentence.split("\\s+");
+            for (int i = 0; i < words.length; i++){
+                words[i] = words[i].replaceAll("^\\w", "");
+            }
         } else {
             System.out.println("Invalid Input!");
         }
